15efc7902ddbacf6501bd18244174dbdc79f8341
yunnus <122170438+yunnus143@users.noreply.github.com>
2023-05-09 10:10:34 +0530

    Initial commit

---
+++ controllers/userControllers.js
@@ -0,0 +1,26 @@
+const users   =require("../models/user.js");
+
+/*
+Post request json file structure
+
+
+    obj =  {
+        "name":name,
+        "email":email,
+        "password": password
+    }
+
+ */
+
+//You need to complete the route of user register
+//you need to register the user and return the id assign to the user.
+//you will get error if user mail allready exist in that case you need to return 404 status with err message that you get.
+//to look the user schema look ../models/user.js
+
+const registerUser =async (req, res) => {
+
+    //Write you code here
+
+}
+
+module.exports = { registerUser };
